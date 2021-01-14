0a772b384602a89e38dfc61d6c4745271eda0edd
Josefiino <74570753+Josefiino@users.noreply.github.com>
2021-01-14 19:27:48 -0300

    Add files via upload

---
+++ sub_rename.py
@@ -0,0 +1,58 @@
+from PyQt5 import QtCore, QtGui, QtWidgets, uic
+import sys, os, re
+
+
+class Renamer_ui(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = uic.loadUi('main_ui.ui', self)
+
+
+        self.installed_folder_path = os.getcwd()
+        self.lineEdit_folder.setText(self.installed_folder_path)
+
+        self.vid_ext = self.lineEdit_video_format.text()
+        self.sub_ext = self.lineEdit_subs_format.text()
+
+        self.ui.pushButton_detect.clicked.connect(self.get_file_list)
+        self.show()
+
+
+    def get_file_list(self):
+        self.plainTextEdit_video_list.clear()
+        self.plainTextEdit_subs_list.clear()
+        self.label_info.setText(f'Reading files...')
+        vid_arr = []
+        subs_arr = []
+        arr = os.listdir(self.lineEdit_folder.text())
+        for i, name in enumerate(arr):
+            if name.endswith('.' + self.vid_ext):
+                vid_arr.append(name)
+                self.plainTextEdit_video_list.appendPlainText(name)
+            elif name.endswith('.' + self.sub_ext):
+                subs_arr.append(name)
+                self.plainTextEdit_subs_list.appendPlainText(name)
+        self.label_info.setText(f'Finished reading files')
+        self.lists = vid_arr, subs_arr
+        self.pushButton_rename.setEnabled(True)
+        self.pushButton_rename.clicked.connect(
+            lambda ch: self.rename_files(self.lists))
+
+    def rename_files(self, lists):
+        if len(lists[0]) == len(lists[1]):
+            self.label_info.setText(f'Renaming files')
+            for counter, item in enumerate(lists[0]):
+                new = re.sub(f"{self.vid_ext}", f"{self.sub_ext}", item)
+                os.rename((self.lineEdit_folder.text() + '\\' + lists[1][counter]), (self.lineEdit_folder.text() + '\\' + new))
+            self.get_file_list()
+            self.pushButton_rename.setEnabled(False)
+            self.label_info.setText(f'Renamed {len(lists[0])} files')
+        else:
+            self.label_info.setText(f"Error: Number of files don't match")
+
+
+
+app = QtWidgets.QApplication(sys.argv)
+
+ui_window = Renamer_ui()
+app.exec_()
